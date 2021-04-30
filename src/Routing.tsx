3553d5490ec99cc32bef69d6cf5829b38b831663
miyazawayamato <yamato8781@gmail.com>
2021-04-30 20:44:54 +0900

    ルーティング設定

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,6 +1,11 @@
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import TopPage from './components/top/TopPage';
 import Menu from './components/menu/Menu';
+import Customer from './components/customer/Customer';
+import Delivery from './components/delivery/Delivery';
+import History from './components/history/History';
+import Products from './components/productInfo/Products';
+import User from './components/user/User';
 
 const Routing = () => {
     return(
@@ -9,6 +14,11 @@ const Routing = () => {
             <Switch>
                 <Route exact path="/" component={TopPage}></Route>
                 <Route path="/mainmenu" component={Menu}></Route>
+                <Route path="/customers" component={Customer }></Route>
+                <Route path="/derivery" component={Delivery}></Route>
+                <Route path="/history" component={History}></Route>
+                <Route path="/products" component={Products}></Route>
+                <Route path="/users" component={User}></Route>
             </Switch>
             </Router>
         </div>
