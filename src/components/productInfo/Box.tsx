e64a55c61dff9b2adea7f4ce3e4110e65033b07c
miyazawayamato <yamato8781@gmail.com>
2021-05-01 20:37:11 +0900

    box

---
+++ src/components/productInfo/Box.tsx
@@ -0,0 +1,19 @@
+
+
+
+const Box = () => {
+    return(
+        <div>
+            <form action="">
+                <input type="text" value="name" name="name"/>
+                <input type="number" value="123" name=""/>
+                <input type="number" value="456"/>
+                <button>更新</button>
+                <button>削除</button>
+            </form>
+            
+        </div>
+    );
+}
+
+export default Box
