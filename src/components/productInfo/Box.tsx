fe120b2fa6a79f6f9fedab9156f3b3439e9fbfe3
miyazawayamato <yamato8781@gmail.com>
2021-05-01 20:53:19 +0900

    box

--- src/components/productInfo/Box.tsx
+++ src/components/productInfo/Box.tsx
@@ -5,9 +5,9 @@ const Box = () => {
     return(
         <div>
             <form action="">
-                <input type="text" value="name" name="name"/>
-                <input type="number" value="123" name=""/>
-                <input type="number" value="456"/>
+                <input id="name" type="text" value="name" name="name"/>
+                <input id="stock" type="number" value="123" name="stock"/>
+                <input id="price" type="number" value="456" name="price"/>
                 <button>更新</button>
                 <button>削除</button>
             </form>
