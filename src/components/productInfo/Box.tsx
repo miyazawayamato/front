86a428ad42fa9942f365da9250f5bd9fa467d631
miyazawayamato <yamato8781@gmail.com>
2021-05-03 08:50:33 +0900

    5/3

--- src/components/productInfo/Box.tsx
+++ src/components/productInfo/Box.tsx
@@ -1,18 +1,51 @@
+import React, { useState } from "react";
+
+type Props = {
+    id: string;
+    name: string;
+    stock: string;
+    price: string;
+}
 
 
-
-const Box = () => {
+const Box:React.FC<Props> = ({id, name, stock, price}) => {
+    
+    const [values, setValues] = useState({name: name, stock : stock, price: price});
+    
+    
+    const valuesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const eValue:string  = e.target.value
+        const eName = e.target.name
+        setValues({ ...values, [eName] : eValue });
+        // console.log(values.name);
+        // console.log(values.stock);
+    }
+    
+    const putData = () => {
+        
+        const postData = {
+            id:id,
+            name:values.name,
+            email:values.stock,
+            body:values.price
+        };
+        
+        console.log(postData)
+        
+    }
+    const deleteData = () => {
+        
+        
+    }
+    
     return(
-        <div>
-            <form action="">
-                <input id="name" type="text" value="name" name="name"/>
-                <input id="stock" type="number" value="123" name="stock"/>
-                <input id="price" type="number" value="456" name="price"/>
-                <button>更新</button>
-                <button>削除</button>
-            </form>
-            
-        </div>
+        <tr>
+            <td><input type="text" defaultValue={values.name} onChange={valuesChange} name="name"/></td>
+            <td><input  type="number" defaultValue={stock} onChange={valuesChange} name="stock"/></td>
+            <td><input  type="number" defaultValue={price} onChange={valuesChange} name="price"/></td>
+            <td><button onClick={putData}>更新</button></td>
+            <td><button onClick={deleteData}>削除</button></td>
+        </tr>
     );
 }
 
--- src/components/productInfo/Products.tsx
+++ src/components/productInfo/Products.tsx
@@ -1,4 +1,4 @@
-
+import Box from "./Box";
 //カウント機能(スピナー機能)
 //商品名と金額と個数
 //保存ボタン(失敗のメッセージ)と削除ボタン(モーダル)
@@ -6,7 +6,28 @@
 const Products = () => {
     return(
         <div>
-            <input type="text"/>
+            <table>
+                <tbody>
+                    <Box 
+                        id="1"
+                        name="商品A"
+                        stock="23"
+                        price="324"
+                    />
+                    <Box 
+                        id="2"
+                        name="商品B"
+                        stock="2334"
+                        price="994"
+                    />
+                    <Box 
+                        id="3"
+                        name="商品C"
+                        stock="7"
+                        price="300"
+                    />
+                </tbody>
+            </table>
         </div>
     );
 }
