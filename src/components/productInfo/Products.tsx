d9526d4b6b8f9302492b6f41bfb62ba6def7c65c
miyazawayamato <yamato8781@gmail.com>
2021-05-03 20:58:34 +0900

    5/3

--- src/components/productInfo/Products.tsx
+++ src/components/productInfo/Products.tsx
@@ -1,9 +1,34 @@
 import Box from "./Box";
+import Modal from "./Modal";
+import React, { useState } from "react";
+
 //カウント機能(スピナー機能)
 //商品名と金額と個数
 //保存ボタン(失敗のメッセージ)と削除ボタン(モーダル)
 
+
+//
 const Products = () => {
+    
+    const [name, changeName] = useState<string>("")
+    //idと名前も 
+    const openModal = (id : string, name : string) => {
+        
+        console.log(id)
+        changeName(name)
+        
+    }
+    const deleteData = () => {
+        
+        const deleteData = {
+            
+            name:name,
+            body:"duhf"
+            
+        }
+        console.log(deleteData)
+    }
+    
     return(
         <div>
             <table>
@@ -13,21 +38,25 @@ const Products = () => {
                         name="商品A"
                         stock="23"
                         price="324"
-                    />
+                        func={openModal}
+                        />
                     <Box 
                         id="2"
                         name="商品B"
                         stock="2334"
                         price="994"
-                    />
+                        func={openModal}
+                        />
                     <Box 
                         id="3"
                         name="商品C"
                         stock="7"
                         price="300"
+                        func={openModal}
                     />
                 </tbody>
             </table>
+            <Modal name={name} deledata={deleteData} />
         </div>
     );
 }
