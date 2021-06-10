3124126b49e38b871ec3e77ed19912d52135fd4a
miyazawayamato <yamato8781@gmail.com>
2021-06-10 20:15:33 +0900

    6/10

--- src/functions/ApiDelete.ts
+++ src/functions/ApiDelete.ts
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const ApiDelete = (url : string) => {
     
-    // return axios.delete(url, data);
-    
     axios.delete(url).then(res => {
         console.log(res.data);
         return res;
