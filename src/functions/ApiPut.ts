a2c8f59f9441462f63da789a19fdd9fef5df6b65
miyazawayamato <yamato8781@gmail.com>
2021-06-20 22:25:37 +0900

    6/20

--- src/functions/ApiPut.ts
+++ src/functions/ApiPut.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 const ApiPut = (url : string, data : object) => {
     
     axios.put(url, data).then(res => {
-        // console.log(res.data);
         return res;
     })
 }
